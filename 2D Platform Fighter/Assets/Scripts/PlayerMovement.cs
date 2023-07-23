c8706680ac96508c15e66d9c7e1d2f804eaf6091
youpgames <51041912+youpgames@users.noreply.github.com>
2023-07-23 23:09:24 +0200

    Created basic horizontal player movement

--- 2D Platform Fighter/Assets/Scripts/PlayerMovement.cs
+++ 2D Platform Fighter/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float speed; //speed float + editable in Unity editor
+    private Rigidbody2D body; 
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
-
+    
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y); //Horizontal player movement, the velocity gets influenced by the input axis (value between -1 and 1)
     }
 }
