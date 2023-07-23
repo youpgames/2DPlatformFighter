9283c37a7fe20bd8ea423ad43fced3a2eace0158
youpgames <51041912+youpgames@users.noreply.github.com>
2023-07-23 23:42:02 +0200

    Added a jump
    issue: the jumpcount does not work yet due to the button pressed / button boolean problem.

--- 2D Platform Fighter/Assets/Scripts/PlayerMovement.cs
+++ 2D Platform Fighter/Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed; //speed float + editable in Unity editor
+    [SerializeField] private float speed; //speed float + editable in Unity editor due to SerializeField
+    [SerializeField] private float jumpheight; // jump height float + editable in Unity editor
+    [SerializeField] private int jumpcount; // amount of jumps a player can do before needing to touch the ground
     private Rigidbody2D body; 
 
     private void Awake()
@@ -20,5 +22,11 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y); //Horizontal player movement, the velocity gets influenced by the input axis (value between -1 and 1)
+        if (Input.GetKey(KeyCode.Space)) // check for player input (can be improved by not checking on every frame)
+            if (jumpcount > 0) //check jump count, if above 0 the player jumps
+               {
+                    body.velocity = new Vector2(body.velocity.x, jumpheight); // Jump implementation
+                    jumpcount = jumpcount - 1; // Decreases the jump by 1 (currently bugged due to the pressed/not pressed on update)
+               }
     }
 }
